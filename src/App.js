bbe719445824cdc162e80828c3e81e2cdfd5e922
yokoya-adriano <adriano_yokoya@hotmail.com>
2022-11-30 18:40:16 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import Topbar from "./scenes/global/Topbar"
+import Sidebar from "./scenes/global/Sidebar"
+import Dashboard from "./scenes/dashboard"
+
+function App() {
+    const [theme, colorMode] = useMode()
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <div className="app">
+                    <main className='content'>
+                        <Topbar />
+                    </main>
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
+}
+
+export default App;
